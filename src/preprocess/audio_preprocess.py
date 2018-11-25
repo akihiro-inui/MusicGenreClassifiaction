f393f62c4ea1d7e10d7049d6c51bb83ac482f165
inuinana <mail@akihiroinui.com>
2018-11-25 23:05:29 +0100

    add: new audio preprocess function

--- src/preprocess/audio_preprocess.py
+++ src/preprocess/audio_preprocess.py
@@ -12,59 +12,69 @@ from utils.audio_util import AudioUtil
 
 
 class AudioPreProcess:
-
-    # Initialization
-    def __init__(self, input_audio_file: str):
+    def __init__(self, frame_time: float, overlap_rate: float, window_type: str):
         """
-        Constractor for audio pre-processing
-        :param input_audio_file: file path to the input audio file
-        """
-        self.filename = input_audio_file
-
-    def read_audio_file(self):
-        """
-        Read audio file
-        :return Dictionary of audio file parameters {key:name of information, value: parameters}
-        """
-        if AudioUtil.is_wav_file(self.filename):
-            return AudioUtil.audio_read(self.filename)
-
-    def framing(self, frame_time: float, overlap_rate: float) -> tuple:
-        """
-        Segmentation forthe input audio file with the given frame length
         :param frame_time: time for framing in ms (e.g. 46.44)
         :param overlap_rate: overlap between frames
+        :param window_type: type of window
+        """
+        self.frame_time = frame_time
+        self.overlap_rate = overlap_rate
+        self.window_type = window_type
+
+    def read_audio_file(self, filename: str) -> dict:
+        """
+        Read audio file
+        :param filename: name of input audio file
+        :return Dictionary of audio file parameters {key:name of information, value: parameters}
+        """
+        if AudioUtil.is_wav_file(filename):
+            return AudioUtil.audio_read(filename)
+
+    def framing(self, audio_dict: dict) -> tuple:
+        """
+        Segmentation for the input audio file with the given frame length
+        :param audio_dict: audio data dictionary {key:name of information, value: parameters}
+                                                 e.g. key: sampling_rate, value: int
         :return tuple of the segmented audio file
         """
-        # Read audio file
-        audio_extracted = self.read_audio_file()
-
-        # Samples in one frame
-        one_frame_samples = math.floor((frame_time / 1000) * audio_extracted["sampling_rate"])
-        self.one_frame_samples = one_frame_samples
+        # Calculate samples in one frame
+        one_frame_samples = math.floor((self.frame_time / 1000) * audio_dict["sampling_rate"])
 
         # Calculate samples in frame step
-        step_samples = math.floor(one_frame_samples * overlap_rate)
+        step_samples = math.floor(one_frame_samples * self.overlap_rate)
 
         # Calculate number of frames
-        frame_number = int(math.floor((len(audio_extracted["data"]) - step_samples) / step_samples))
-        print("Frame length is {0} with {1}% overlap".format(one_frame_samples, int(100 * overlap_rate)))
-        print("{} frames from the input audio file".format(frame_number))
+        frame_number = int(math.floor((len(audio_dict["data"]) - step_samples) / step_samples))
 
         # Store framed audio input into tuple
-        framed_audio_tuple = {}
+        framed_audio_list = []
         for frame in range(frame_number):
-            framed_audio_tuple[frame] = audio_extracted["data"][frame * step_samples:(frame + 1) * step_samples]
-        return framed_audio_tuple
+            framed_audio_list.append(audio_dict["data"][frame * step_samples:(frame + 1) * step_samples])
+        return tuple(framed_audio_list)
 
-    def windowing(self, input_audio: list, window_type: str, window_length: int) -> list:
+    def windowing(self, framed_audio_tuple: tuple, window_type: str = "hamm") -> tuple:
         """
         Apply window to the audio signal
-        :param input_audio: input audio to be windowed
+        :param framed_audio_tuple: input audio to be windowed
         :param window_type: type of window
-        :param window_length: length of window
-        :return list of windowed audio signal
+        :return tuple of windowed audio signal
         """
+        # Size of window
+        window_size = len(framed_audio_tuple[0])
+
+        # Apply window
+        windowed_audio_list = []
         if window_type is "hamm":
-            windowed_signal = input_audio * np.hamming(window_length)
-        return windowed_signal
+            for frame in range(len(framed_audio_tuple)):
+                windowed_audio_list.append(framed_audio_tuple[frame] * np.hamming(window_size))
+        return tuple(windowed_audio_list)
+
+    def apply(self, input_filename: str) -> tuple:
+        """
+        Apply pre-processing to input audio file.
+        :param input_filename: input audio file
+        :return tuple of pre-processed audio signal
+        """
+        return self.windowing(self.framing(self.read_audio_file(input_filename)))
+
