4f17eb4758f50077421c8bb6be885f8287b86bef
Akihiro Inui <mail@akihiroinui.com>
2018-12-10 22:55:15 +0100

    Delete test_script.py

--- unitTest/test_script.py
+++
@@ -1,8 +0,0 @@
-audio_file = "/Users/akihiro/Desktop/projects/genre_classification/MusicGenreClassifiaction/data/Blues/Blues blues.00000.wav"
-from utils.audio_utils import AudioUtil
-
-def test_audio_read():
-
-
-if __name__ == "__main__":
-    test_audio_read()
