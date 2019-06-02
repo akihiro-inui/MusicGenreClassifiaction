488e3cb7aeadda28064485e315d4cb85ce29485d
Akihiro Inui <akihiro@MacBook-Pro-4.local>
2019-06-03 00:31:50 +0200

    Add: Updated logistic regression

--- backend/src/classifier/logistic_regression.py
+++ backend/src/classifier/logistic_regression.py
@@ -5,11 +5,12 @@ Created on 17 May 2019
 @author: Akihiro Inui
 """
 
+import os
 import torch
 import numpy as np
 from torch import nn, optim
-import pandas as pd
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 
 class LogisticRegression:
@@ -51,7 +52,7 @@ class LogisticRegression:
 
         # To log losses
         losses = []
-        for epoch in range(100):
+        for epoch in range(10000):
             # Delete gradient value calculated in previous epoch
             optimizer.zero_grad()
 
@@ -59,8 +60,7 @@ class LogisticRegression:
             predicted_label = model(train_data)
 
             # Calculate MSE loss
-            a = torch.max(predicted_label, 1)[1]
-            loss = loss_fn(torch.max(predicted_label, 1)[1], onehot_train_label)
+            loss = loss_fn(predicted_label, onehot_train_label)
             loss.backward()
 
             # Update gradient
@@ -68,6 +68,11 @@ class LogisticRegression:
 
             # Log loss
             losses.append(loss.item())
+
+        # Visualize losses
+        if visualize is True:
+            plt.plot(losses)
+            plt.show()
 
         return model
 
@@ -79,7 +84,7 @@ class LogisticRegression:
         """
         # Load model if it exists
         assert os.path.exists(model_file_name), "Given model file does not exist"
-        return load_model(model_file_name)
+        return torch.load(model_file_name, map_location="cpu")
 
     def save_model(self, model, output_directory: str):
         """
@@ -87,7 +92,7 @@ class LogisticRegression:
         :param  model: trained model
         :param  output_directory: output directory path
         """
-        model.save(os.path.join(output_directory, "gru.h5"))
+        torch.save(model.state_dict(), os.path.join(output_directory, "logistic_regression.prm"), pickle_protocol=4)
 
     def test(self, model, test_data, test_label):
         """
@@ -98,9 +103,10 @@ class LogisticRegression:
         :return result of test
         """
         # One hot encode
-        onehot_test_label = pd.get_dummies(test_label)
+        onehot_test_label = torch.tensor(np.array(test_label), dtype=torch.long)
         # Make predictions and output result
-        return model.evaluate(test_data, onehot_test_label)[1]
+        predicted = torch.max(model(torch.tensor(np.array(test_data), dtype=torch.float32)), 1)[1]
+        return (predicted == onehot_test_label).sum().item()/len(test_label)
 
     def predict(self, model, target_data):
         """
@@ -110,4 +116,4 @@ class LogisticRegression:
         :return prediction array with probability
         """
         # Make prediction to the target data
-        return model.predict(target_data)
+        return model(torch.tensor(np.array(target_data), dtype=torch.float32))
